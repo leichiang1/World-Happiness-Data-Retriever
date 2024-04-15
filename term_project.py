23a715c794f2b66f04e7df79e3ecb5925e38ccde
Lucas C <147535814+leichiang1@users.noreply.github.com>
2024-04-15 11:38:40 -0500

    Update term_project.py

--- term_project.py
+++ term_project.py
@@ -18,16 +18,80 @@ class stats():
         self.variables = ["Overall Rank", "Happiness Score", "GDP Per Capita", "Social Support", 
                           "Healthy Life Expectancy", "Freedom to Make Life Choices", "Generosity", "Perceptions of Corruption"]
         with open("2019.csv", "r") as data:
-            for line in csv.reader(data):
+            csvreader = csv.reader(data)
+            next(csvreader)
+            for line in csvreader:
                 self.hash_table[line[1]] = [int(line[0])] + list(map(float, line[2:]))
 
+        self.main_menu()
     def main_menu(self):
         """Displays the main menu and controls the overall program based on user input"""
-        pass
+        valid_choices = ["0", "1", "2", "3", "4", "5", "6", "7", "Q"]
 
-    def get_mean(self, category):
-        vals = [list_of_values[index] for list_of_values in self.hash_table.values()]
-        return sum(vals) / len(self.hash_table)
+        print("#########################################################")
+        print("Welcome to World Happiness Data Explorer Project")
+        print("#########################################################")
+        print("Options: (Type in the Corresponding Number to Choose an Option")
+        print("0: Display All Data")
+        print("1. Search For a Country")
+        print("2. Get Mean for a Category")
+        print("3. Get Median for a Category")
+        print("4. Get Standard Deviation for a Category")
+        print("5. Get Top K countries in a Category")
+        print("6. Get Bottom K countries in a Category")
+        print("7. Display Category Information")
+        print("Q. QUIT")
+        print("#########################################################")
+
+        while(True):
+            user_input = input()
+            if user_input not in valid_choices:
+                continue
+
+            if user_input == "0":
+                print(self.display_all())
+
+            if user_input == "1":
+                self.search()
+            
+            if user_input == "2":
+                mean_choices = ["0", "1", "2", "3", "4", "5", "6", "7" "M"]
+                print("#########################################################")
+                print("Choose a Category to Find Mean (Type Corresponding Number)")
+                print("0. Happiness Score")
+                print("1. GDP Per Capita")
+                print("2. Social Support")
+                print("3. Healthy Life Expectancy")
+                print("4. Freedom to Make Life Choices")
+                print("5. Generosity")
+                print("6. Perceptions of Corruption")
+                print("M. Main Menu")
+                print("#########################################################")
+                user_input = input()
+                while(True):
+                    if user_input not in mean_choices:
+                        continue
+                    if user_input == "M":
+                        self.main_menu()
+                    self.get_mean(int(user_input))
+            
+            if user_input == "3":
+                self.get_median()
+            
+            if user_input == "4":
+                self.get_std_dev()
+            
+            if user_input == "5":
+                self.get_top_k()
+
+            if user_input == "6":
+                self.get_bottom_k()
+            
+            if user_input == "7":
+                self.display_cat_info()
+            
+            if user_input == "Q":
+                return
 
     def display_all(self):
         pd.options.display.max_rows = 1000
@@ -36,6 +100,16 @@ class stats():
         df = pd.read_csv(file)
         pd.options.display.max_columns = len(df.columns)
         return df
+    
+    def display_cat_info(self):
+        pass
+
+    def get_mean(self, index):
+        vals = [list_of_values[index] for list_of_values in self.hash_table.values()]
+        print()
+        print("Mean Value For Category:", sum(vals) / len(self.hash_table))
+        print()
+        self.main_menu()
 
     def search(self):
         pass
@@ -46,7 +120,16 @@ class stats():
     def get_std_dev(self):
         pass
 
+    def get_top_k(self):
+        pass
+
+    def get_bottom_k(self):
+        pass
+
     def merge_sort(self):
+        pass
+
+    def merge(self):
         pass
 
 class min_heap():
@@ -58,5 +141,3 @@ class max_heap():
 
 if __name__ == "__main__":
     s = stats()
-
-# Overall rank,Country or region,Score,GDP per capita,Social support,Healthy life expectancy,Freedom to make life choices,Generosity,Perceptions of corruption
